ce70e553cb3d4fde87747a06a8592565a838c88b
MotingLiuu <moting.liuu@gmail.com>
2025-05-28 11:18:39 +0900

    reconstituted parallel_pretokenize

--- cs336_basics/bpe_tokenizer.py
+++ cs336_basics/bpe_tokenizer.py
@@ -20,22 +20,17 @@ class BPETokenizer:
     
     def train(self, input_path: str):
         pass
-        
-    def parallel_pretokenize(self, input_path: str) -> Dict[str, int]:
-        def process_chunk(input_path: str, sta: int, end: int):
-            with open(input_path, 'rb') as f:
-                f.seek(sta)
-                chunk = f.read(end - sta)
-            return self.pretokenize_binary(chunk)
-        
+    
+    @staticmethod
+    def parallel_pretokenize(input_path: str, pattern, special_tokens: Optional[List[str]] = [r'<endoftext>']) -> Dict[str, int]:
         token_counts = Counter()
         with open(input_path, 'rb') as f:
             boundaries = find_chunk_boundaries(
-                f, 32, "<|endoftext|>".encode("utf-8")
+                f, 128, "<|endoftext|>".encode("utf-8")
             )
-        subprocess_args = [(input_path, sta, end) for sta, end in zip(boundaries[:-1], boundaries[1:])]
+        subprocess_args = [(input_path, pattern, special_tokens, sta, end) for sta, end in zip(boundaries[:-1], boundaries[1:])]
         with Pool(8) as p:
-            results = p.map(process_chunk, subprocess_args)
+            results = p.starmap(BPETokenizer.parallel_pretokenize_auxiliary, subprocess_args)
         for r in results:
             token_counts.update(r)
         return token_counts    
@@ -52,16 +47,20 @@ class BPETokenizer:
         return token_counts
     
     @staticmethod
-    def parallel_pretokenize_auxiliary(input_path: str, pattern, special_tokens: Optional[list[str]] = [r'<|endoftext|>']):
-        pass
+    def parallel_pretokenize_auxiliary(input_path: str, pattern, special_tokens: Optional[list[str]] = [r'<|endoftext|>'], sta: int = 0, end: int = 0):
+        with open(input_path, 'rb') as f:
+            f.seek(sta)
+            chunk = f.read(end - sta)
+        return BPETokenizer.pretokenize_binary(chunk, pattern, special_tokens)
                 
 if __name__ == '__main__':
     BPE = BPETokenizer(30, [r'<|endoftext|>'])
-    DATA_PATH = os.path.join(os.path.dirname(__file__), '../../data/TinyStoriesV2-GPT4-valid.txt')
+    DATA_PATH = os.path.join(os.path.dirname(__file__), '../../data/TinyStoriesV2-GPT4-train.txt')
     DATA_PATH = os.path.abspath(DATA_PATH)
     start = time.time()
-    with open(DATA_PATH, 'rb') as f:
-        token_counts = BPETokenizer.pretokenize_binary(f.read(), BPE.PAT)
+    #with open(DATA_PATH, 'rb') as f:
+    #    token_counts = BPETokenizer.pretokenize_binary(f.read(), BPE.PAT)
+    token_counts = BPETokenizer.parallel_pretokenize(DATA_PATH, BPE.PAT)
     end = time.time()
     for idx, (token, count) in enumerate(token_counts.most_common()):
         if idx < 100:
