0c10d2dc38120e372aa4e8604e7799c9ef56c823
MotingLiuu <moting.liuu@gmail.com>
2025-06-01 21:56:42 +0900

    added experiments about TinyStories and owt

---
+++ experiments/bpe_train_TinyStories.py
@@ -0,0 +1,27 @@
+from cs336_basics import BPETokenizer
+from pathlib import Path
+import time
+import json
+
+DATA_PATH = str((Path(__file__).parent / '../../data/TinyStoriesV2-GPT4-valid.txt').resolve())
+bpe_TinyStories = BPETokenizer(2000, [r'<|endoftext|>'])
+start = time.time()
+bpe_TinyStories.train(DATA_PATH)
+end = time.time()
+
+vocab_to_save = {
+    idx: token_bytes.decode('utf-8', errors='replace')
+    for idx, token_bytes in bpe_TinyStories.vocab.items()
+}
+
+with open('vocab_TinyStories.json', 'w', encoding='utf-8') as f:
+    json.dump(vocab_to_save, f, ensure_ascii=False, indent=2)
+
+longest_tokens = sorted(
+    bpe_TinyStories.vocab.items(),
+    key=lambda x: len(x[1]),
+    reverse=True
+)[:100]
+
+print(f'train on TinyStories took {end - start}s\n')
+print(f'longest_tokens: {longest_tokens}\n')
