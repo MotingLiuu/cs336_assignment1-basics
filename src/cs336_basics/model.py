0bab479aac7788949773e8b5a378337e57bc8569
MotingLiuu <moting.liuu@gmail.com>
2025-08-01 07:29:15 +0900

    completed adamw

--- src/cs336_basics/model.py
+++ src/cs336_basics/model.py
@@ -274,6 +274,53 @@ class SGD(torch.optim.Optimizer):
                 p.data -= lr / math.sqrt(t + 1) * grad
                 state["t"] = t + 1
         return loss
+    
+class AdamW(torch.optim.Optimizer):
+    def __init__(
+        self,
+        params: Iterable[nn.Parameter],
+        lr: float=1e-3,
+        betas: tuple[float, float] = (0.9, 0.999),
+        eps: float = 1e-8,
+        weight_decay: float = 0.01,
+    ):
+        if lr < 0:
+            raise ValueError(f"Invalid learning rate: {lr}")
+        defaults = {
+            "lr": lr,
+            "betas": betas,
+            "eps": eps,
+            "weight_decay": weight_decay,
+        }
+        super().__init__(params, defaults)
+        
+    def step(self, closure: Optional[Callable] = None):
+        loss = None if closure is None else closure()
+        for group in self.param_groups:
+            lr = group["lr"]
+            betas = group["betas"]
+            eps = group["eps"]
+            weight_decay = group["weight_decay"]
+            
+            for p in group["params"]:
+                if p.grad is None:
+                    continue
+                
+                state = self.state[p]
+                m = state.get("m", torch.zeros_like(p.data))
+                v = state.get("v", torch.zeros_like(p.data))
+                t = state.get("t", 1)
+                grad = p.grad.data
+                m = betas[0] * m + (1 - betas[0]) * grad
+                v = betas[1] * v + (1 - betas[1]) * grad * grad
+                alpha_t = lr * (math.sqrt(1 - betas[1] ** t)) / (1 - betas[0] ** t)
+                p.data -= alpha_t * m / (torch.sqrt(v) + eps)
+                if weight_decay > 0:
+                    p.data -= lr * weight_decay * p.data
+                state["m"] = m
+                state["v"] = v
+                state["t"] = t + 1
+        return loss
         
 
 def scaled_dot_product_attention(
