6970b2430c8b8d4b6355d8ecc00e84d021b6f1f4
MotingLiuu <moting.liuu@gmail.com>
2025-08-01 07:29:35 +0900

    completed adapter for adamw

--- tests/adapters.py
+++ tests/adapters.py
@@ -531,7 +531,7 @@ def get_adamw_cls() -> type[torch.optim.Optimizer]:
     """
     Returns a torch.optim.Optimizer that implements AdamW.
     """
-    raise NotImplementedError
+    return model.AdamW
 
 
 def run_get_lr_cosine_schedule(
