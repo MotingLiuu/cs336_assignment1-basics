d5a9cb1c0c20d40b0f92c30542b8e413eb7a5037
MotingLiuu <moting.liuu@gmail.com>
2025-08-10 21:38:22 +0900

    deleted useless logging of model.py

--- src/cs336_basics/model.py
+++ src/cs336_basics/model.py
@@ -15,11 +15,6 @@ import math
 
 
 logger = logging.getLogger(__name__)
-logging.basicConfig(
-    level=logging.INFO, 
-    format="%(asctime)s %(levelname)s %(name)s: %(message)s",
-    datefmt="%Y-%m-%d %H:%M:%S",
-)
 
 class Linear(nn.Module):
     def __init__(
